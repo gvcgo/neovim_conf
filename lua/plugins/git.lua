f0b21150124bf0c21e603b5f8bba3853643f18cc
moqsien <moqsien@foxmail.com>
2026-02-22 13:14:24 +0800

    update

---
+++ lua/plugins/git.lua
@@ -0,0 +1,38 @@
+return {
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			signs = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signs_staged = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signs_staged_enable = true,
+
+			signcolumn = true,
+			current_line_blame = true,
+
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol",
+				delay = 500,
+				ignore_whitespace = false,
+			},
+			current_line_blame_formatter = "<author>, <author_time:%R> • <summary>",
+
+			numhl = false,
+			linehl = false,
+		})
+	end,
+}
