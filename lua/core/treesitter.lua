ac56485f6db577d97f415d21f3a71c77943f0bda
moqsien <moqsien@foxmail.com>
2026-02-22 12:07:18 +0800

    update

--- lua/core/treesitter.lua
+++ lua/core/treesitter.lua
@@ -1,5 +1,16 @@
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "lua", "rust", "javascript", "go", "markdown", "bash", "json", "typescript", "yaml", "proto" },
+	pattern = {
+		"lua",
+		"rust",
+		"javascript",
+		"go",
+		"markdown",
+		"bash",
+		"json",
+		"typescript",
+		"yaml",
+		"proto",
+	},
 	callback = function()
 		vim.treesitter.start()
 	end,
