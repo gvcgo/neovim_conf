315e8f06df7fefd1b7f436ecf828791a58c2639a
eric.wei <eric.wei@vibe.us>
2026-03-16 13:47:13 +0800

    update

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -7,6 +7,7 @@ return {
 	config = function()
 		local ai_model = vim.env.AI_MODEL
 		local ai_end_point = vim.env.AI_END_POINT
+		local mc = require("minuet.config")
 		require("minuet").setup({
 			provider = "openai_compatible",
 			cmp = {
@@ -23,12 +24,18 @@ return {
 					-- model = "LongCat-Flash-Thinking-2601"
 					-- end_point = "https://api.longcat.chat/openai/v1/chat/completions",
 					-- api_key = "LONGCAT_API_KEY",
+
+					system = mc.default_system,
+					few_shots = mc.default_few_shots,
+					chat_input = mc.default_chat_input,
 					model = ai_model,
 					end_point = ai_end_point,
 					api_key = "AI_API_KEY",
 					name = "default",
 					optional = {
-						max_tokens = 4096,
+						-- max_tokens = 4096,
+						stop = { "\n", "```", "---", "\r\n", " " },
+						max_tokens = 2048,
 					},
 				},
 			},
