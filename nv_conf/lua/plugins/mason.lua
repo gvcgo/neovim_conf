ff337d0a9a0627819e96beba1c7157fb547ce257
eric.wei <eric.wei@vibe.us>
2026-09-02 10:42:25 +0800

    update

--- nv_conf/lua/plugins/mason.lua
+++ nv_conf/lua/plugins/mason.lua
@@ -55,13 +55,13 @@ return {
 			},
 		})
 
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*.py",
-			callback = function()
-				vim.lsp.buf.format({ name = "ruff" })
-				vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
-			end,
-		})
+		-- vim.api.nvim_create_autocmd("BufWritePre", {
+		-- 	pattern = "*.py",
+		-- 	callback = function()
+		-- 		vim.lsp.buf.format({ name = "ruff" })
+		-- 		vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
+		-- 	end,
+		-- })
 
 		require("mason-lspconfig").setup(opts)
 
